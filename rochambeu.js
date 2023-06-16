bda2795650d4e6c81150f4d0163439d9d4f48d08
carafelix <heroprotagonist32@gmail.com>
2023-06-15 21:10:14 -0400

    Generate cpu choice n able to play one round games

--- rochambeu.js
+++ rochambeu.js
@@ -6,6 +6,7 @@
 // Cpu choices must only be initialized after the user already selected a weapon of choice
 // so no cheating its allowed prior. Since the machine selects its own weapon on random, there is no problem on letting it select after
 // Draws should not count towards the scores but should be registered in the total rounds count.
+// Would do multiple iterations to get the flow of it
 
 // Ideas: 
 // Make the user able to select if he wants to play a golden goal round, a best of 3 , a Fto5 or a Fto10.
@@ -22,7 +23,7 @@
 // Define the elementary inputs regarding the game                          |   *0
 // Ask the user to input his choice via prompt and normalize the string     |   *1
 // Computer selects a random choice                                         |   *2
-// Determine if it is a Tie or any of the parties won                       |   *3
+// Play and determine if it is a Tie or any of the parties won              |   *3
 // If someome won, score them a point                                       |   *4
 // Reset computer choice and answer for the player next choice              |   *7
 // Loop until the selected amount of score is reached by one of the parties |   *8
@@ -30,12 +31,8 @@
 // ask the user if he want to play another set and loop                     |   *10
 
 
-// *0
-
-const rock = "Rock";
-const paper = "Paper";
-const scissors = "Scissors";
-let cpuChoice;
+// In this first iteration I would not implement the conditional for cpu choice to await for human choice
+// Since I think im missing the point on it and getting a bit stuck, and want to continue. I would revisit.
 
 // normalize function 
 
@@ -47,6 +44,10 @@ function uniformInput(string) {
     return firstCap + excludeFirstLetter;    
 }
 
+const rock = "Rock"
+const paper = "Paper"
+const scissors = "Scissors"
+
 // *1 verb for asking the user for a choice
 
 function getUserChoice() {
@@ -55,8 +56,39 @@ function getUserChoice() {
 
 // *1.1 normalize User Choice
 
-let usrChoice = getUserChoice();
-let usrChoiceClean = uniformInput(usrChoice);
+let usrChoiceStep = getUserChoice();
+let usrChoice = uniformInput(usrChoiceStep);
+
+// *2 Cpu choice
+
+function getCpuChoice(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    const cpuChoice = array[randomIndex];
+    return cpuChoice;
+}
+
+const cpuChoiceArray = [rock, paper, scissors]
+
+cpuChoice = getCpuChoice(cpuChoiceArray)
+
+// Play a game *3
+
+let playAgame = function (usr, cpu) {
+    if (usr == cpu) {
+        return("It's a tie baby!")
+    } else if ((usr == paper && cpu == rock) 
+    || (usr == rock && cpu == scissors)
+    || (usr == scissors && cpu == paper)) {
+        return(`You Win! strike with ${usr} and Cpu got hit hard with his ${cpu}, poor machine`)
+    } else {
+        return(`Cpu wins again baby! Your ${usr} is nothing compared to the Cpu ${cpu}`)
+    }
+}
+
+console.log(playAgame(usrChoice, cpuChoice));
+
+
+
 
 
 
@@ -105,22 +137,15 @@ let usrChoiceClean = uniformInput(usrChoice);
 
 
 
-// Cpu Array random
 
-const cpuWepArray = [rock, paper, scissors]
-
-function getCpuChoice(arr) {
-const randomIndex = Math.floor(Math.random() * arr.length);
-const cpuChoice = arr[randomIndex];
-return cpuChoice;
-}
-
-cpuChoice = getCpuChoice(cpuWepArray)
 
 
 
 
 // trash can: 
+
+// code was inside functions that got deleted, it was okay for avoiding getting wrong inputs
+// and letting the user know but was getting pretty messy
 
 
 // if (string == null || string == undefined) {
@@ -141,3 +166,23 @@ cpuChoice = getCpuChoice(cpuWepArray)
 //     usrChoice = undefined
 //     alert("What are you doing??? Going to battle with words?")
 // }
+
+// Writing this code after implementing *2 since I also haved another way to solve it
+
+
+
+// function cpuCpu () {
+//     return Math.floor((Math.random() * 3)+1)
+// }
+
+// let cpuGuess = cpuCpu();
+
+//     if (cpuGuess == 1) {
+//         cpuGuess = rock
+//     } else if (cpuGuess == 2) {
+//         cpuGuess = paper
+//     } else {
+//         cpuGuess = scissors
+//     }
+
+
