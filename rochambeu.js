3a165846631e53f28073cf4658576c27cc9a2930
carafelix <heroprotagonist32@gmail.com>
2023-06-21 01:21:06 -0400

    Add get random-org api call, commits on obj.js

--- rochambeu.js
+++ rochambeu.js
@@ -122,6 +122,38 @@ function playAgame (rounds) {
     }
 }  
 
+const apiObj = {
+    "jsonrpc": "2.0",
+    "method": "generateIntegers",
+    "params": {
+        "apiKey": "aa28fec6-cdad-47f8-b471-cc8ee106298f",
+        "n": 1,
+        "min": 1,
+        "max": 3,
+        "replacement": true,
+        "base": 10,
+        "pregeneratedRandomization": null
+    },
+    "id": 27714
+}
+
+let getRandom = [];
+
+let randomApi = async function (){
+   await fetch("https://api.random.org/json-rpc/4/invoke", {
+        method: "POST",
+        body: JSON.stringify(apiObj),
+        headers: {"Content-type": "application/json; charset=UTF-8"}   
+    })
+        .then(res => res.json())
+        .then((data) =>{
+             getRandom.unshift(data)
+        });
+}
+
+// getRandom[0]["result"]["random"]["data"][0]
+
+
 
 
 
