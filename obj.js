b2cab3a20907834805fb8d7a76f87091bfd70028
carafelix <heroprotagonist32@gmail.com>
2023-06-26 22:18:45 -0400

    apicall obj update

--- obj.js
+++ obj.js
@@ -1,8 +1,8 @@
-const obj = {
+const apiFetchObj = {
     "jsonrpc": "2.0",
     "method": "generateIntegers",
     "params": {
-        "apiKey": "aa28fec6-cdad-47f8-b471-cc8ee106298f",
+        "apiKey": "aa28fec6-cdad-47f8-b471-cc8ee106298f", // I know this shouldn't be public but idk how to keep it secret in a front-end enviroment
         "n": 1,
         "min": 1,
         "max": 3,
@@ -13,18 +13,19 @@ const obj = {
     "id": 27714
 }
 
-let getRandom = [];
+const randomOrgCpu = []; // 'let' should be easier to clear it; const with: randomOrgCpu.length = 0 || while(A.length > 0) {A.pop();}
 
-const randomApi = async function (){
+async function getRandomApiCall() {
    await fetch("https://api.random.org/json-rpc/4/invoke", {
         method: "POST",
-        body: JSON.stringify(obj),
+        body: JSON.stringify(apiFetchObj),
         headers: {"Content-type": "application/json; charset=UTF-8"}   
     })
         .then(res => res.json())
         .then((data) =>{
-             getRandom.unshift(data)
+            randomOrgCpu.unshift(data)
         });
 }
 
 // getRandom[0]["result"]["random"]["data"][0]
+
