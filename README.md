0960231fca1edfde82c0072d6b1d2b5aa3780fda
carafelix <heroprotagonist32@gmail.com>
2023-06-28 13:15:14 -0400

    clean js file from comments

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Sansukumi-ken
 Simple rock paper scissors game
+
+## General Plan; goals, input, expected result, rules, ideas.
+
+The objective of this proyect is to allow the user to play a simple
+Rock-Paper-Scissors game against the computer; first via text input-output form, and later with a GUI
+
+Cpu choices must only be initialized after the user already selected a weapon of choice
+so no cheating its allowed prior. Since the machine selects its own weapon on random, there is no problem on letting it select after
+Draws should not count towards the scores but should be registered in the total rounds count.
+Would do multiple iterations to get the flow of it
+
+## Ideas 
+- Make the user able to select if he wants to play a golden goal round, a best of 3 , a Fto5 or a Fto10.
+- Implement random.org API using fetch() on cpuChoice, so it is truly random.
+- Hestiation is defeat, maybe with a counter like when kids slap their fist into the other hand for count. setTimeout Function. 
+- Maybe implement a Voice-Recognition Library to allow the user to shout like a kid to the computer hahaha.
+- GUI visualitation of rounds/score record.
--- js/rps/rochambeu.js
+++ js/rps/rochambeu.js
@@ -1,36 +1,3 @@
-// General Plan; goals, input, expected result, rules, ideas.
-
-// The objective of this proyect is to allow the user to play a simple
-// Rock-Paper-Scissors game against the computer; first via text input-output form, and later with a GUI
-
-// Cpu choices must only be initialized after the user already selected a weapon of choice
-// so no cheating its allowed prior. Since the machine selects its own weapon on random, there is no problem on letting it select after
-// Draws should not count towards the scores but should be registered in the total rounds count.
-// Would do multiple iterations to get the flow of it
-
-// Ideas: 
-// Make the user able to select if he wants to play a golden goal round, a best of 3 , a Fto5 or a Fto10.
-// Implement random.org API using fetch() on cpuChoice, so it is truly random.
-// Hestiation is defeat, maybe with a counter like when kids slap their fist into the other hand for count. setTimeout Function. 
-// Maybe implement a Voice-Recognition Library to allow the user to shout like a kid to the computer hahaha.
-// GUI visualitation of rounds/score record.
-
-
-// Pseudocode
-
-// Ask the user if he wants to play a game of Rochambeu, and to wich score  |   *5
-// Give the user and the machine a counter for score (or lifes)             |   *6
-// Define the elementary inputs regarding the game                          |   *0
-// Ask the user to input his choice via prompt and normalize the string     |   *1
-// Computer selects a random choice                                         |   *2
-// Play and determine if it is a Tie or any of the parties won              |   *3
-// If someome won, score them a point                                       |   *4
-// Reset computer choice and answer for the player next choice              |   *7
-// Loop until the selected amount of score is reached by one of the parties |   *8
-// Display the winning partie                                               |   *9
-// ask the user if he want to play another set and loop                     |   *10
-
-
 // In this first iteration I would not implement the conditional for cpu choice to await for human choice
 // Since I think im missing the point on it and getting a bit stuck, and want to continue. I would revisit.
 
