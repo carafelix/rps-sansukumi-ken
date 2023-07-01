49c953f48891d3a43d65e90b137e46129c9722e3
carafelix <heroprotagonist32@gmail.com>
2023-07-01 00:04:34 -0400

    add random music play test

--- js/bg-music.js
+++ js/bg-music.js
@@ -1,3 +1,18 @@
+const audio = document.querySelectorAll('audio');
+const audioArr = Array.from(audio);
+
 function randomTime(){
-   return Math.floor(Math.random() * 15)
+   return Math.floor(Math.random() * 5)
 }
+
+function randomIndex(){
+    return Math.floor(Math.random() * 11)
+}
+
+function bgMusic(){
+    setTimeout(() => {
+        audioArr
+    }, randomTime());
+}
+
+// document.addEventListener('load', audioArr[randomIndex()].play() ) 
